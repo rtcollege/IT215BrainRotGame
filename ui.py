baaedf12eec2c5d97aaa755e3aa0ca2c9c077386
R T <41290888-rtiedemancolleg@users.noreply.replit.com>
2025-04-17 23:45:14 +0000

    Restored to 'd5aa52a4eec55578c432b3900b1a250514aa4404'
    Replit-Restored-To: d5aa52a4eec55578c432b3900b1a250514aa4404

--- ui.py
+++ ui.py
@@ -35,8 +35,7 @@ class UI:
         # Volume slider
         self.volume_text = self.font.render("Volume:", True, "white")
         text_height = self.volume_text.get_height()
-        slider_y = int(250 * SCALE_Y)
-        self.volume_text_rect = self.volume_text.get_rect(topleft=(50, slider_y))
+        self.volume_text_rect = self.volume_text.get_rect(topleft=(50, 250 - text_height/2)) #Corrected line
         slider_width = int(200 * SCALE_X)
         slider_height = int(20 * SCALE_Y)
         slider_handle_width = int(20 * SCALE_X)
@@ -46,7 +45,7 @@ class UI:
         self.volume = 50
         self.volume_slider = pygame.Rect(
             WINDOW_WIDTH/2 - self.slider_width/2 + (self.volume * self.slider_width/100),
-            slider_y - slider_handle_height/2 + slider_height/2,
+            int(245 * SCALE_Y),
             slider_handle_width,
             slider_handle_height
         )
@@ -55,9 +54,10 @@ class UI:
         self.volume_rect = pygame.Rect(WINDOW_WIDTH/2 - slider_width/2, int(250 * SCALE_Y), slider_width, slider_height)
 
         # Difficulty buttons
-        diff_y = int(400 * SCALE_Y)
+        diff_y = 400
         self.difficulty_text = self.font.render("Difficulty:", True, "white")
-        self.difficulty_text_rect = self.difficulty_text.get_rect(topleft=(50, diff_y))
+        text_height = self.difficulty_text.get_height()
+        self.difficulty_text_rect = self.difficulty_text.get_rect(topleft=(50, diff_y - text_height/2))
         button_spacing = int(200 * SCALE_X)  # Consistent spacing between buttons
         self.easy_button = Button(None, (WINDOW_WIDTH/2 - button_spacing, int(diff_y * SCALE_Y)), "Easy", self.font, "white", "#b68f40")
         self.medium_button = Button(None, (WINDOW_WIDTH/2, int(diff_y * SCALE_Y)), "Medium", self.font, "white", "#b68f40")
