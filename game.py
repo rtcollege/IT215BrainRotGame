530c19d61aef10d7868fef1c69715f6d45517240
R T <41290888-rtiedemancolleg@users.noreply.replit.com>
2025-04-24 21:23:32 +0000

    Restored to '6cb95e98b5e59c96cc2cf88db0c7c0d020777866'
    Replit-Restored-To: 6cb95e98b5e59c96cc2cf88db0c7c0d020777866

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+
 import pygame
 from ballsim import BallSimulation
 
@@ -6,7 +7,7 @@ class Game:
         self.display_surface = pygame.display.get_surface()
         self.ui = ui
         self.ball_sim = BallSimulation()
-
+        
     def update(self, dt):
         if self.ui.current_scene == 'gameplay' and not self.ui.is_paused:
             # Draw container box and ball
