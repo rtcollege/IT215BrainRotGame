9ee6458226ecb27e1a0c38b1cf0f01011c7a856f
rtcollege <150183565+rtcollege@users.noreply.github.com>
2025-05-01 16:31:01 +0000

    Restored to '058b037aec8e0605443c2aec5f2ccc85a2ab7767'
    Replit-Restored-To: 058b037aec8e0605443c2aec5f2ccc85a2ab7767

--- ballsimulation.py
+++ ballsimulation.py
@@ -5,7 +5,6 @@ from button import Button
 from ball import Ball
 from circle import Circle
 from particle import Particle
-import math
 from math import sin, cos, radians, hypot, atan2, degrees
 import random
 import pygame.gfxdraw
